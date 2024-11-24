7d025ece8d954684f0995587060d8e4044add300
PotatoBreadCrumbs <131428209+PotatoBreadCrumbs@users.noreply.github.com>
2024-11-24 01:45:08 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1313,5 +1313,10 @@ def view_saved_items():
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local development
+    app.run(host="0.0.0.0", port=port, debug=True)
+
